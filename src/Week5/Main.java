c48283cabf82a62e83b3a9b5adf8c6f6208b6408
User <tunji4physics@gmail.com>
2023-08-21 13:08:38 +0100

    Create objects of the Circle, Square and Rectangle -- Also demonstrate Liskov Sustitution principle substituting Square 'child' class for its parent class, 'Shape'

---
+++ src/Week5/Main.java
@@ -0,0 +1,29 @@
+package Week5;
+
+import java.awt.Color;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		//Creating objects of Rectangle, Square and Circle
+		
+		Rectangle redRect =  new Rectangle(Color.red, 2.5, 2.5, 3.0);
+		
+		/*
+		 * Creating blueSquare demonstrates the Liskov Substitution Principle:
+		 * That every subclass or  derived class should be substitutable for their base or parent class.
+		 * The derived class should always obey the contract of the interface it implements
+		 */
+		 Shape blueSquare = new Square(Color.blue, 1.5, 3.5);
+		 
+		 Circle pinkCircle = new Circle(Color.pink,2.5, 3);
+		 
+		 System.out.println(redRect.getHeight());
+		 System.out.println(blueSquare.getLineTickness());
+		 System.out.println(pinkCircle.getLineColour());
+		 pinkCircle.setLineColour(Color.CYAN);
+		 System.out.println(pinkCircle.getLineColour());
+	}
+	
+
+}
