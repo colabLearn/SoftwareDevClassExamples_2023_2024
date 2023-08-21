5b93951ebc99c147d302d1c64222dd13b41295b9
User <tunji4physics@gmail.com>
2023-08-21 11:53:37 +0100

    Creating the class Shape.java --This class extract the common fields and methods from the Rectangle, Circle and Square.java class. Shape will become the parent class for Circle, Rectangle and Square classes.

---
+++ src/Week5/Shape.java
@@ -0,0 +1,39 @@
+package Week5;
+
+import java.awt.Color;
+
+public class Shape {
+	//Shape has the common fields in Circle, Rectangle and Square.java
+	private Color  lineColour;      
+	private double lineTickness;
+	
+		
+	public Shape() {
+		
+	}
+
+	public Shape(Color lineColour, double lineTickness) {
+		
+		this.lineColour = lineColour;
+		this.lineTickness = lineTickness;
+	}
+
+	public Color getLineColour() {
+		return lineColour;
+	}
+
+	public void setLineColour(Color lineColour) {
+		this.lineColour = lineColour;
+	}
+
+	public double getLineTickness() {
+		return lineTickness;
+	}
+
+	public void setLineTickness(double lineTickness) {
+		this.lineTickness = lineTickness;
+	}  
+	
+	
+
+}
