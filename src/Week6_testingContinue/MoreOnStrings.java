36402b87f2eea0c1b23540118fe51d3d6aeb77a3
User <tunji4physics@gmail.com>
2023-08-29 12:09:18 +0100

    Demonstrating JVM String pool --> only 1 compy of the same string is kept in the pool, can be referenced by many variables

--- src/Week6_testingContinue/MoreOnStrings.java
+++ src/Week6_testingContinue/MoreOnStrings.java
@@ -6,18 +6,22 @@ public class MoreOnStrings {
 		String myName = "Tunji";
 		String myName2 = "Tunji";
 		String myName3 = new String("Tunji");
+		String myName4 = "Tunji";
 	
 		System.out.println(myName);
 		System.out.println(myName2);
 		System.out.println(myName3);
 		
-		System.out.println(myName==myName2);
+		System.out.println((myName==myName2) && (myName4==myName2)&& (myName==myName4));
 		System.out.println(myName==myName3);
 		System.out.println(myName2==myName3);
+		System.out.println(myName4==myName3);
 		
 		System.out.println(myName.equals(myName2));
 		System.out.println(myName.equals(myName3));
 		System.out.println(myName2.equals(myName3));
+		System.out.println(myName4.equals(myName3));
+		System.out.println(myName2.equals(myName4));
 	}
 
 }
