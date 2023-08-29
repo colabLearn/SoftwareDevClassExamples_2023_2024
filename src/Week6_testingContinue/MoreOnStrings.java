f418a8f926813274c8d1201c82ef1a506edd17eb
User <tunji4physics@gmail.com>
2023-08-29 12:00:21 +0100

    Create a string object and demonstate with checking equality with == and .equals

--- src/Week6_testingContinue/MoreOnStrings.java
+++ src/Week6_testingContinue/MoreOnStrings.java
@@ -5,10 +5,19 @@ public class MoreOnStrings {
 	public static void main(String[] args) {
 		String myName = "Tunji";
 		String myName2 = "Tunji";
+		String myName3 = new String("Tunji");
 	
+		System.out.println(myName);
+		System.out.println(myName2);
+		System.out.println(myName3);
 		
 		System.out.println(myName==myName2);
+		System.out.println(myName==myName3);
+		System.out.println(myName2==myName3);
+		
 		System.out.println(myName.equals(myName2));
+		System.out.println(myName.equals(myName3));
+		System.out.println(myName2.equals(myName3));
 	}
 
 }
