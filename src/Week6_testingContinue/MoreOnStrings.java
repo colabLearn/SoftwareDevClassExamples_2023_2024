a3be48053831ba14287ba7d3420d75e38a56bcc6
User <tunji4physics@gmail.com>
2023-08-29 11:49:38 +0100

    Explaing more on String --> creating string object and checking string equality

---
+++ src/Week6_testingContinue/MoreOnStrings.java
@@ -0,0 +1,14 @@
+package Week6_testingContinue;
+
+public class MoreOnStrings {
+	
+	public static void main(String[] args) {
+		String myName = "Tunji";
+		String myName2 = "Tunji";
+	
+		
+		System.out.println(myName==myName2);
+		System.out.println(myName.equals(myName2));
+	}
+
+}
